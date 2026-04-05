faab9465c4a2b1a66639e5339068e0d5238f68c0
Jorge Nuñez <nunez.jorge@hotmail.com>
2026-04-05 18:20:10 -0300

    Añade el contenedor de espacios en la HomeScreen, con sus estilos

--- src/Screens/HomePageScreen/HomePageScreen.jsx
+++ src/Screens/HomePageScreen/HomePageScreen.jsx
@@ -24,7 +24,7 @@ function HomePageScreen() {
                         </div>
 
                         {/* Primera sección */}
-                        <div className="hp-sign-in-conainer">
+                        <div className="hp-sign-in-container">
                             <div className="workspaces-container">
                                 <div className="my-workspaces">
                                     {/* Título de la sección */}
@@ -54,10 +54,10 @@ function HomePageScreen() {
                                                     Listo para iniciar
                                                 </span>
                                                 {/* Desde acá arranca el componente a crear */}
-                                                {/* Falta crear la página del link */}
-                                                <Link to={`/`} className='workspace-preview-container'>
-                                                    <div className="workspace-list-container">
-                                                        <img src={"https://a.slack-edge.com/80588/img/avatars-teams/ava_0026-88.png"} alt="Imagen del espacio de trabajo" />
+                                                {/* TODO: Falta crear la página del link */}
+                                                <Link to={`/`} className='workspace-list-item'>
+                                                    <div className="workspace-list-content">
+                                                        <img className='workspace-icon' src={"https://a.slack-edge.com/80588/img/avatars-teams/ava_0026-88.png"} alt="Imagen del espacio de trabajo" />
                                                         <div className="workspace-details">
                                                             <div className="workspaces-name">
                                                                 Trabajo Final UTN
@@ -68,14 +68,14 @@ function HomePageScreen() {
                                                                 </span>
                                                             </div>
                                                         </div>
-                                                    </div>
-                                                    <div className="workspace-list-item-button">
-                                                        <span className="workspace-list-item-button-text">
-                                                            Iniciar
-                                                        </span>
-                                                        <svg className="workspace-list-item-button-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M9 18L15 12L9 6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                                        </svg>
+                                                        <div className="workspace-list-item-button">
+                                                            <span className="workspace-list-item-button-text">
+                                                                Iniciar
+                                                            </span>
+                                                            <svg className="workspace-list-item-button-icon" width="24" height="24" viewBox="0 0 12 14" xmlns="http://www.w3.org/2000/svg">
+                                                                <path d="M6 0C6.41421 0 6.75 0.335786 6.75 0.75V11.377L10.707 7.23242C10.9929 6.93292 11.4679 6.92139 11.7676 7.20703C12.0671 7.49292 12.0786 7.96793 11.793 8.26758L6.54297 13.7676C6.40145 13.9158 6.20496 14 6 14C5.79504 14 5.59855 13.9158 5.45703 13.7676L0.207031 8.26758C-0.0786086 7.96793 -0.0670771 7.49292 0.232422 7.20703C0.532066 6.92139 1.00708 6.93292 1.29297 7.23242L5.25 11.377V0.75C5.25 0.335787 5.58579 7.73111e-07 6 0Z"></path>
+                                                            </svg>
+                                                        </div>
                                                     </div>
                                                 </Link>
                                                 {/* Acá termina el componente */}
