aba08833c4b298353393189bd8af73cad1453a72
Jorge Nuñez <nunez.jorge@hotmail.com>
2026-04-27 00:28:45 -0300

    Modifica los estilos del error al no haber mensajes para que sea visible

--- src/components/ui/MessageListComponent/MessageListComponent.jsx
+++ src/components/ui/MessageListComponent/MessageListComponent.jsx
@@ -1,14 +1,17 @@
+// Styles
+import './MessageListComponent.css'
+// Components
 import MessageComponent from '../MessageComponent/MessageComponent'
 
 const MessageListComponent = ({ messages, loading, error, messagesEndRef }) => {
     if (loading && !messages) {
-        return <div className='siderbar-item-component-error messages-error'>Cargando mensajes...</div>
+        return <div className='chat-error messages-error'>Cargando mensajes...</div>
     }
     if (error) {
-        return <div className='siderbar-item-component-error messages-error'>Error al cargar los mensajes</div>
+        return <div className='chat-error messages-error'>Error al cargar los mensajes</div>
     }
     if (messages?.length === 0) {
-        return <div className='siderbar-item-component-error messages-error'>No hay mensajes en este canal</div>
+        return <div className='chat-error messages-error'>No hay mensajes en este canal</div>
     }
 
     return (
